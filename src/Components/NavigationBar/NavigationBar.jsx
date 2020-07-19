07f0f3d2ff2c7c87b37b46d857082902f67e4872
Utsav Virani <56069399+Utsav-Virani@users.noreply.github.com>
2020-07-19 17:25:44 +0530

    changes in nav bar

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -131,17 +131,6 @@ export default class NavigationBar extends Component {
                                         </Link>
                                     </li>
                                     <li className="nav-item">
-                                        <Link className="nav-link" to="about" style={{ color: navFontColor }}>
-                                            <div className="font">
-                                                <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-info-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                    <path fillRule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z" />
-                                                    <circle cx="8" cy="4.5" r="1" />
-                                                </svg>
-                                            </div>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
                                         <Link className="nav-link" to="shop" style={{ color: navFontColor }}>
                                             <div className="font">
                                                 <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-shop-window" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -153,6 +142,8 @@ export default class NavigationBar extends Component {
                                             </div>
                                         </Link>
                                     </li>
+                                    
+                                    
                                     <li className="nav-item">
                                         <Link className="nav-link" to="/page" style={{ color: navFontColor }}>
                                             <div className="font">
@@ -162,6 +153,19 @@ export default class NavigationBar extends Component {
                                             </div>
                                         </Link>
                                     </li>
+
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="about" style={{ color: navFontColor }}>
+                                            <div className="font">
+                                                <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-info-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fillRule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z" />
+                                                    <circle cx="8" cy="4.5" r="1" />
+                                                </svg>
+                                            </div>
+                                        </Link>
+                                    </li>
+                                    
                                     <li className="nav-item">
                                         <Link className="nav-link" to="/contact" style={{ color: navFontColor }}>
                                             <div className="font">
