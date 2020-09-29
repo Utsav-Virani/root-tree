3427d5d07fadccc00fc4becec5f17eacc32a2df0
U V <uvvirani004@gmail.com>
2020-09-29 12:39:05 +0530

    work on shopPage

--- src/Components/ShopScreen/ShopScree.jsx
+++ src/Components/ShopScreen/ShopScree.jsx
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 
-class ShopScree extends Component {
-    render() {
-        return (
-            <div className="display-1">
-                Shop
+import Style from './Shop.module.scss';
+import { dataBase } from '../../FireBaseControler/firebaseConfig';
+
+
+export default function ShopScree() {
+
+    const [img, setImg] = useState(null);
+
+    useEffect(() => {
+        const fetchImgData = async () => {
+            const imgData = await dataBase.collection('Fruits').get()
+            setImg(imgData.docs.map(doc => {
+                return doc.data()
+            }))
+        }
+        fetchImgData()
+    }, [])
+
+    return (
+        <React.Fragment>
+            <div className={`${Style.shopCont}`}>
+                <div className={`${Style.fruitCont}`}>
+                    <div className={`${Style.FruitHead}`}>Fruits</div>
+                    <div className={`${Style.froutImgCont} row w-100`}>
+                        {
+                            img ? img.map(data => {
+                                return (
+                                    <div className={`${Style.frtProduct} col-4`}>
+                                        <img src={data.photo} height="150" width="150" alt={data.name} />
+                                        <div><p>{data.name}</p></div>
+                                    </div>
+                                )
+                            }) : null
+                        }
+                    </div>
+                </div>
             </div>
-        );
-    }
+        </React.Fragment>
+    );
 }
-
-export default ShopScree;
