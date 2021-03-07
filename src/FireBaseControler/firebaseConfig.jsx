46e221de2147d2f5487666ea9fa9912d7de7d5e7
Utsav Virani <uvvirani004@gmail.com>
2021-03-07 12:36:08 +0530

    project complete

--- src/FireBaseControler/firebaseConfig.jsx
+++ src/FireBaseControler/firebaseConfig.jsx
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyArOxlK0siYR77oSborEc52owClKLOIKLw",
-  authDomain: "root-tree.firebaseapp.com",
-  databaseURL: "https://root-tree.firebaseio.com",
-  projectId: "root-tree",
-  storageBucket: "root-tree.appspot.com",
-  messagingSenderId: "515177086360",
-  appId: "1:515177086360:web:6659639c9141563244bfed",
-  measurementId: "G-VP6HT9PXVY"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 const config = firebase.initializeApp(firebaseConfig);
